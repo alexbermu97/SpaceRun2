a1464db941aee5b66ee27675556935f54d489d52
Alex Bermúdez <47413101+alexbermu97@users.noreply.github.com>
2020-08-01 02:29:31 +0200

    Add files via upload
    "Assets" and "Project Settings" folders, the basics of every Unity project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    //ESTE SCRIPT CONTROLA EL MOVIMIENTO DEL JUGADOR
+
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    [Range(1f, 5f)]
+    float ySensitivity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        rb.velocity = Vector2.up * v * (ySensitivity * 100f) * Time.deltaTime;
+    }
+}
